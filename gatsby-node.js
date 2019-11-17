79d805411582c34e4fd1c8e97c8166a5351cf693
Patrik Keinonen <pate@lygon.net>
2019-11-17 12:56:28 +0100

    boilderplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  stage,
+  loaders,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    devtool: 'source-map',
+    resolve: {
+      modules: [path.resolve(__dirname, `src`), `node_modules`],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          // use: [loaders.js(), require.resolve('ts-loader')]
+          loader: require.resolve('ts-loader'),
+
+        },
+        {
+          test: /\.tsx?$/,
+          enforce: `pre`,
+          exclude: /(node_modules|cache|public)/,
+          use: [
+            {
+              loader: require.resolve(`tslint-loader`),
+              options: { emitErrors: stage === `build-javascript` }
+            }
+          ]
+        },
+      ]
+    },
+    resolve: {
+      plugins: [new TsconfigPathsPlugin()]
+    }
+  })
+};
+
+exports.resolvableExtensions = () => ['.ts', '.tsx', '.js', '.jsx'];
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <h1>Hello from TypeScript!</h1>
+  );
+};
+
+export default IndexPage;
